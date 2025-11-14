37627faf4c57698d50b38f576a3460b66b6805b2
Shaun Donnelly <shaun.t.e.donnelly@gmail.com>
2025-11-14 15:17:21 +0000

    fix: update pyproject.toml to include setuptools-scm; modify __init__.py for version handling

--- src/windy_api/__init__.py
+++ src/windy_api/__init__.py
@@ -4,7 +4,12 @@ Windy API: Python Windy API package for interacting with the Windy API.
 
 from __future__ import annotations
 
-from importlib.metadata import version
+try:
+    from windy_api._version import version as __version__
+except ImportError:
+    # Package not installed or setuptools-scm not run yet
+    from importlib.metadata import version
+
+    __version__ = version(__name__)
 
 __all__ = ("__version__",)
-__version__ = version(__name__)
